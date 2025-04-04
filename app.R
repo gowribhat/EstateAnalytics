afd502bcc178d0d71caa05b4f3b4c14dae08e4b4
BiKiLoEB <noelloweiern96@gmail.com>
2025-04-04 14:10:59 +0800

    Update app.R

--- app.R
+++ app.R
@@ -2,4 +2,11 @@ source("global.R")
 source("ui.R")
 source("server/server.R")
 
+<<<<<<< Updated upstream
 shinyApp(ui, server)
+=======
+shinyApp(ui = ui, server = server)
+
+HDB <- readRDS("hdb.rds")
+priv <- readRDS("ura_private.rds")
+>>>>>>> Stashed changes
