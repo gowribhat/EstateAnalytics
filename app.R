8de378be2573c41e278431b20f51bb78f4c46f8b
BiKiLoEB <noelloweiern96@gmail.com>
2025-04-04 16:42:52 +0800

    Update app.R

--- app.R
+++ app.R
@@ -2,16 +2,20 @@ source("global.R")
 source("ui.R")
 source("server/server.R")
 
+HDB <- readRDS("C:/Users/User/R-4.4.3/Project/data/hdb.rds")
+priv <- readRDS("C:/Users/User/R-4.4.3/Project/data/ura_private.rds")
+
 ui <- fluidPage()
 shinyApp(ui, server)
 server <- function(input,output){
+  
   output$plotMap <- renderLeaflet(
     {
       leaflet() %>% setView(lng = 103.9355, lat = 1.359087,
                             zoom = input$zoomlevel) %>% 
         addTiles() %>% 
-        addMarkers(lat = 1.359087, lng = 103.9355,
-                   popup = "My Old Home")
+        addMarkers(lat = HDB$latitude[1], lng = HDB$longitude[1],
+                   popup = paste(HDB$block[1],HDB$street_name[1]))
     }
   )
 }
