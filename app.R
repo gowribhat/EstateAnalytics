c15dd50cdf6ced57e07e2eb617ffcde101a85832
BiKiLoEB <noelloweiern96@gmail.com>
2025-04-05 11:03:13 +0800

    Update app.R

--- app.R
+++ app.R
@@ -2,6 +2,8 @@ source("global.R")
 source("ui.R")
 source("server/server.R")
 
+area <- fromJSON("district_and_planning_area.geojson")
+area <- as.data.frame(area)
 HDB <- readRDS("C:/Users/User/R-4.4.3/Project/data/hdb.rds")
 priv <- readRDS("C:/Users/User/R-4.4.3/Project/data/ura_private.rds")
 
@@ -14,8 +16,8 @@ server <- function(input,output){
       leaflet() %>% setView(lng = mean(HDB$longitude), lat = mean(HDB$latitude),
                             zoom = input$zoomlevel) %>% 
         addTiles() %>% 
-        addMarkers(lat = HDB$latitude[1], lng = HDB$longitude[1],
-                   popup = paste(HDB$block[1],HDB$street_name[1]))
+        addMarkers(lat = HDB$latitude[1:1000], lng = HDB$longitude[1:1000],
+                   popup = paste(HDB$block[1:1000],HDB$street_name[1:1000]))
     }
   )
 }
@@ -23,7 +25,7 @@ server <- function(input,output){
 ui <- fluidPage(
   sliderInput(inputId = "zoomlevel",
               label = "Map Zooming Level",
-              value = 14,
+              value = 12,
               min = 1,
               max = 20),
   leafletOutput(outputId = "plotMap")
