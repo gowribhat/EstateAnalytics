fb4562f9d4b08bac30137a17421241521d34a842
BiKiLoEB <noelloweiern96@gmail.com>
2025-04-06 10:56:17 +0800

    Update app.R

--- app.R
+++ app.R
@@ -2,26 +2,11 @@ source("global.R")
 source("ui.R")
 source("server/server.R")
 
-area <- fromJSON("district_and_planning_area.geojson")
-area <- as.data.frame(area)
+# Loading data
+area <- st_read("C:/Users/User/R-4.4.3/Project/data/district_and_planning_area.geojson")
 HDB <- readRDS("C:/Users/User/R-4.4.3/Project/data/hdb.rds")
-priv <- readRDS("C:/Users/User/R-4.4.3/Project/data/ura_private.rds")
 
-ui <- fluidPage()
-shinyApp(ui, server)
-server <- function(input,output){
-  
-  output$plotMap <- renderLeaflet(
-    {
-      leaflet() %>% setView(lng = mean(HDB$longitude), lat = mean(HDB$latitude),
-                            zoom = input$zoomlevel) %>% 
-        addTiles() %>% 
-        addMarkers(lat = HDB$latitude[1:1000], lng = HDB$longitude[1:1000],
-                   popup = paste(HDB$block[1:1000],HDB$street_name[1:1000]))
-    }
-  )
-}
-
+# UI definition
 ui <- fluidPage(
   sliderInput(inputId = "zoomlevel",
               label = "Map Zooming Level",
@@ -30,5 +15,43 @@ ui <- fluidPage(
               max = 20),
   leafletOutput(outputId = "plotMap")
 )
-shinyApp(ui = ui, server = server)
 
+# Server definition
+server <- function(input, output) {
+
+  # Reactive to summarize HDB data by planning_area
+  HDB_clean <- reactive({
+    HDB %>%
+      group_by(planning_area) %>%
+      summarise(price = mean(resale_price, na.rm = TRUE))
+  })
+  
+  
+  # Reactive to merge HDB_area with area
+  HDB_merged <- reactive({
+    merge(HDB_clean(), area, by.x = "planning_area", by.y = "planning_area")  # Ensure a left join
+  })
+  
+  # Render the map with the merged data
+  output$plotMap <- renderLeaflet({
+    HDB_merged_data <- HDB_merged()
+    
+    # Create a color palette for the map
+    pal <- colorNumeric(palette = "Reds", domain = HDB_merged_data$price)
+    
+    # Create the leaflet map
+    leaflet(data = st_as_sf(HDB_merged_data)) %>%
+      addTiles() %>%
+      addPolygons(
+        weight = 2,
+        stroke = TRUE,
+        smoothFactor = 0.1,
+        fillOpacity = 0.8,
+        color = ~pal(price),
+        popup = ~planning_area
+      )
+  })
+}
+
+# Run the app
+shinyApp(ui = ui, server = server)
