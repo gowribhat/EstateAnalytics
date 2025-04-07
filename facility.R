11cc4697c982ca413e681e3e47fb1cfd029e901c
BiKiLoEB <noelloweiern96@gmail.com>
2025-04-07 10:46:46 +0800

    Update facility.R

--- facility.R
+++ facility.R
@@ -5,3 +5,11 @@ mrt <- readRDS(paste0(resource_path,"LRT_MRT.rds"))
 park <- readRDS(paste0(resource_path,"parks_data.rds"))
 sch <- readRDS(paste0(resource_path,"schools.rds"))
 mrt <- readRDS(paste0(resource_path,"LRT_MRT.rds"))
+
+hdb <- readRDS(paste0(resource_path,"hdb.rds"))
+priv <- readRDS(paste0(resource_path,"ura_private.rds"))
+
+distance <- function(x,y){
+  d <- sqrt((x$longitude-y$longitude)^2+(x$latitude-y$latitude)^2)
+  return(111.1*d)
+}
