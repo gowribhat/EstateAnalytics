1b7cb33942b6fb18cb19b68a109b5efdbc9c6ca5
BiKiLoEB <noelloweiern96@gmail.com>
2025-04-07 10:41:31 +0800

    Facility location

---
+++ facility.R
@@ -0,0 +1,7 @@
+resource_path <- "data/"
+childcare <- readRDS(paste0(resource_path,"childcares.rds"))
+gym <- readRDS(paste0(resource_path,"gyms_data.rds"))
+mrt <- readRDS(paste0(resource_path,"LRT_MRT.rds"))
+park <- readRDS(paste0(resource_path,"parks_data.rds"))
+sch <- readRDS(paste0(resource_path,"schools.rds"))
+mrt <- readRDS(paste0(resource_path,"LRT_MRT.rds"))
